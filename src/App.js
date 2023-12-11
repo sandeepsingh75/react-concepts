2afd97affa328b1fcde5add097f0208ed22fb208
sandeep singh <sandeepsinghrana100@gmail.com>
2023-12-11 13:23:17 +0530

    use props in class components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './components/Home.js';
 import Profile from './components/Profile.js';
+import Profile1 from './components/profile1.js';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <h1> Hello world</h1>
       <Home name="Sandeep Singh" />
       <Profile />
+      <Profile1 name="sanjay" mark="80" />
     </div>
   );
 }
