dbf27c56b612e723c019b2212a547853caf0d10b
sandeep singh <sandeepsinghrana100@gmail.com>
2023-12-11 12:59:32 +0530

    function calledn on click event

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,11 @@
 const Profile =()=>{
+    const red=()=>{
+        alert("click me text clicked")
+    }
     return(
         <>
         <h1>Create Fat arrow function</h1>
-        
+        <h2 onClick={red}> click me </h2>
         </>
     )
 }
