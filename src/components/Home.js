c623381d33792992507f291eb9ee2f96210c5087
sandeep singh <sandeepsinghrana100@gmail.com>
2023-12-18 19:18:17 +0530

    useEffect hook use

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,42 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from'react';
 
-class Home extends React.Component{
-    constructor(){
-        super();
-        this.state={
-            name:'sandeep singh',
-            email:'san@gmail.com',
-            count:1
-        }
+const Home =(props)=>{
+    const [count, setCount] = useState(0);
+    
+    
+    const add=()=>{
+        setCount(count+1)
     }
-    updatestate()
-    {
-        this.setState({
-            name:'sanjay'
-        })
+    const sub=()=>{
+        setCount(count-1)
     }
-    add(){
-        this.setState({
-            count: this.state.count+1,
-        })
-    }
-    sub(){
-        this.setState({
-            count: this.state.count-1,
-        })
-    }
-    render(){
-        return(
-            <div>
-                <h3> name: {this.state.name} </h3>
-                <h3> email: {this.state.email} </h3>
-                <h3> count: {this.state.count} </h3>
-                <button onClick={()=>this.updatestate()}> Update Name</button>
-                <button onClick={()=> this.add()} > count increment </button>
-                <button onClick={()=> this.sub()} > count decrement </button>
-            </div>
-        )
-    }
+    return(
+        <div>
+        <h3>Home component</h3>
+        <p>Name: {props.name}</p>
+        <p> Count : {count} </p>
+        <button onClick={()=>add()} >increment</button>
+        <button onClick={()=>sub()}>decrement</button>
+        
+        </div>
+    )
 }
 
 export default Home;
